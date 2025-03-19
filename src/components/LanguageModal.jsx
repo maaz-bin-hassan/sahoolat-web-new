682700f2954bfff11b748e28c4e1e2ff8298a0ea
Fahad Shahzad <director@novasyncdynamics.com>
2025-03-19 23:27:38 +0500

    fix named import

--- src/components/LanguageModal.jsx
+++ src/components/LanguageModal.jsx
@@ -7,7 +7,7 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import useTranslate from "@/hooks/useTranslate";
+import { useTranslate } from "@/hooks/useTranslate";
 
 const LanguageModal = () => {
   const { langChange } = useTranslate();
