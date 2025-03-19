b527709808751b07dd52746b4e7c5a306361be1a
Fahad Shahzad <director@novasyncdynamics.com>
2025-03-19 23:14:12 +0500

    type of window undefined

--- src/hooks/useTranslate.jsx
+++ src/hooks/useTranslate.jsx
@@ -1,39 +1,43 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import { getCookie, hasCookie, setCookie } from "cookies-next";
 
-export const useTranslate = () => {
+
+const GoogleTranslate = () => {
   const [selected, setSelected] = useState(null);
 
   useEffect(() => {
-    var addScript = document.createElement("script");
-    addScript.setAttribute(
-      "src",
-      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit",
-    );
-    document.body.appendChild(addScript);
-    window.googleTranslateElementInit = googleTranslateElementInit;
+    if (typeof window !== "undefined") {
+      var addScript = document.createElement("script");
+      addScript.setAttribute("src", "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit");
+      document.body.appendChild(addScript);
+      window.googleTranslateElementInit = googleTranslateElementInit;
 
-    const storedLang = localStorage.getItem("googtrans");
-    if (storedLang) {
-      setSelected(storedLang);
-    } else {
-      setSelected("/auto/en");
+      if (hasCookie("googtrans")) {
+        setSelected(getCookie("googtrans"));
+      } else {
+        setSelected("/auto/en");
+      }
     }
   }, []);
 
   const googleTranslateElementInit = () => {
-    new window.google.translate.TranslateElement({
-      pageLanguage: "auto",
-      autoDisplay: false,
-      includedLanguages: "en,ur",
-    }, "google_translate_element");
+    if (typeof window !== "undefined") {
+      new window.google.translate.TranslateElement({
+        pageLanguage: "auto",
+        autoDisplay: false,
+        includedLanguages: "en,ur,hi,bn,th,fr,ar",
+      }, "google_translate_element");
+    }
   };
 
-  const langChange = (lang) => {
-    setCookie("googtrans", decodeURIComponent(lang));
-    setSelected(lang);
-    window.location.reload();
-  };
 
-  return { langChange, selected };
+  return (
+    <div className="flex items-center">
+      <div id="google_translate_element" className={"hidden"} />
+    </div>
+  );
 };
+
+export default GoogleTranslate;
