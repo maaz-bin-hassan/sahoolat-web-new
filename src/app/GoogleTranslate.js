7588be8a68ad625b4ca06a1586a2508aafaa3eb9
Momin Khan <mominkhan.dev@gmail.com>
2025-03-19 23:20:56 +0500

    add: window undefined error

--- src/app/GoogleTranslate.js
+++ src/app/GoogleTranslate.js
@@ -8,24 +8,28 @@ const GoogleTranslate = () => {
   const [selected, setSelected] = useState(null);
 
   useEffect(() => {
-    var addScript = document.createElement("script");
-    addScript.setAttribute("src", "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit");
-    document.body.appendChild(addScript);
-    window.googleTranslateElementInit = googleTranslateElementInit;
+    if (typeof window !== "undefined") {
+      var addScript = document.createElement("script");
+      addScript.setAttribute("src", "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit");
+      document.body.appendChild(addScript);
+      window.googleTranslateElementInit = googleTranslateElementInit;
 
-    if (hasCookie("googtrans")) {
-      setSelected(getCookie("googtrans"));
-    } else {
-      setSelected("/auto/en");
+      if (hasCookie("googtrans")) {
+        setSelected(getCookie("googtrans"));
+      } else {
+        setSelected("/auto/en");
+      }
     }
   }, []);
 
   const googleTranslateElementInit = () => {
-    new window.google.translate.TranslateElement({
-      pageLanguage: "auto",
-      autoDisplay: false,
-      includedLanguages: "en,ur,hi,bn,th,fr,ar",
-    }, "google_translate_element");
+    if (typeof window !== "undefined") {
+      new window.google.translate.TranslateElement({
+        pageLanguage: "auto",
+        autoDisplay: false,
+        includedLanguages: "en,ur,hi,bn,th,fr,ar",
+      }, "google_translate_element");
+    }
   };
 
   const langChange = (e, lang) => {
