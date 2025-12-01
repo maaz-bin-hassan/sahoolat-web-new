7372bec654bed0ba365d014fc3b0c3470ee6875c
Maaz Bin Hassan <mleghari64@gmail.com>
2025-12-01 23:35:01 +0500

    f

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  basePath: isProd ? "/SahoolatAi" : "",
-  assetPrefix: isProd ? "/SahoolatAi/" : "",
-  output: "export",
   images: {
     unoptimized: true,
   },
